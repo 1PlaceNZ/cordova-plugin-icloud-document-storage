4518ed0bd54f7ebc1a0ae1d1c7504c91e79e17be
Darrell Wu <darrell@1placeonline.com>
2022-06-13 11:37:12 +1200

    added package.lock. remove hook

--- src/ios/hooks/install_entitlements.js
+++ src/ios/hooks/install_entitlements.js
@@ -46,12 +46,16 @@ module.exports = function (context) {
     if (!projFolder || !projName) {
       throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
     }
+    console.error("projFolder: " + projFolder);
+    console.error("projName: " + projName);
 
     var destFile = path.join(iosFolder, projName, 'Resources', projName + '.entitlements');
+    console.error("Project entitlement destFile: " + destFile);
     if (fs.existsSync(destFile)) {
       console.error("File exists, not doing anything: " + destFile);
     } else {
       var sourceFile = path.join(context.opts.plugin.pluginInfo.dir, 'src/ios/resources/iCloud.entitlements');
+      console.error("icloud entitlement file: " + sourceFile);
       fs.readFile(sourceFile, 'utf8', function (err, data) {
 
         if(process.argv.join("|").indexOf("CONTAINER_NAME=") > -1) {
@@ -60,19 +64,21 @@ module.exports = function (context) {
           var config = fs.readFileSync("config.xml").toString()
           var CONTAINER_NAME = getPreferenceValue(config, "CONTAINER_NAME")
         }
+        console.error("CONTAINER_NAME: " + CONTAINER_NAME);
         data.replace(/CONTAINER_NAME/g, CONTAINER_NAME)
 
 
         var resourcesFolderPath = path.join(iosFolder, projName, 'Resources');
         fs.existsSync(resourcesFolderPath) || fs.mkdirSync(resourcesFolderPath);
         fs.writeFileSync(destFile, data);
-
+        console.error('wrote  ' + sourceFile + ' to ' + destFile);
         var projectPath = path.join(projFolder, 'project.pbxproj');
 
         var pbxProject;
         if (context.opts.cordova.project) {
           pbxProject = context.opts.cordova.project.parseProjectFile(context.opts.projectRoot).xcode;
         } else {
+          console.error('use node module xcode');
           pbxProject = xcode.project(projectPath);
           pbxProject.parseSync();
         }
